225f42d190bf1f1ea12c106b0c7d1442559ce880
mhamedhussein52 <102866604+mhamedhussein52@users.noreply.github.com>
2022-08-11 13:59:46 +0200

    commit

--- lib/weather/data/data_source/remote_data_source.dart
+++ lib/weather/data/data_source/remote_data_source.dart
@@ -12,7 +12,6 @@ class RemoteDataSource implements BaseRemoteDataSource{
   Future<WeatherModel?> getWeatherByCountryName(String countryName)async {
     try {
       var response = await Dio().get('${AppConstants.baseUrl}weather?q=$countryName&appid=${AppConstants.apiKey}');
-      print(response);
       return WeatherModel.fromJson(response.data);
     } catch (e) {
       return null;
